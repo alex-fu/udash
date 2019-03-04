17669359899f1cb75b984ec5f0b4afc4a0ed6871
Fu Yifeng <fyfhust@gmail.com>
2019-03-04 23:50:37 +0800

    add dashboard

--- src/main.js
+++ src/main.js
@@ -1,9 +1,31 @@
+// Polyfills
+import 'es6-promise/auto'
+import 'babel-polyfill'
+
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import VeeValidate from 'vee-validate'
+import VuesticPlugin from '@/vuestic-theme/vuestic-plugin'
+import './i18n'
 
 Vue.config.productionTip = false
+
+Vue.use(VuesticPlugin)
+
+// NOTE: workaround for VeeValidate + vuetable-2
+Vue.use(VeeValidate, { fieldsBagName: 'formFields' })
+
+
+router.beforeEach((to, from, next) => {
+  store.commit('setLoading', true)
+  next()
+})
+
+router.afterEach((to, from) => {
+  store.commit('setLoading', false)
+})
 
 new Vue({
   router,
--- src/router.js
+++ src/router.js
@@ -1,13 +1,18 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import AppLayout from './components/admin/AppLayout.vue'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
+      path: '*',
+      redirect: { name: 'dashboard' },
+    },
+    {
+      path: '/home',
       name: 'home',
       component: Home
     },
@@ -18,6 +23,19 @@ export default new Router({
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+    },
+    {
+      path: '/admin',
+      name: 'admin',
+      component: AppLayout,
+      children: [
+        {
+          path: 'dashboard',
+          name: 'dashboard',
+          component: () => import('./components/dashboard/Dashboard.vue'),
+          default: true
+        }
+      ]
     }
   ]
 })
